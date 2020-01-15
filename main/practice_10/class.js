cbc0730898814bd582c9a16a40b4440c152c19ea
Asuu <suqinzheng_0624@163.com>
2020-01-15 23:02:50 +0800

    [zhengsuqin]feat-10: creat a class named Person and a class named Student and a class named teacher and a class named class

--- main/practice_10/class.js
+++ main/practice_10/class.js
@@ -1,1 +1,26 @@
+class Class {
+	constructor(klass) {
+		this.number = klass;
+	}
 
+	getDisplayName() {
+		return `Class ${this.number}`;
+	}
+
+  isIn(student) {
+    return student.klass === this;
+  }
+
+	assignLeader(student) {
+		if (this.isIn(student)) {
+			this.leader = student;
+		} else {
+			console.log("It is not one of us.");
+		}
+	}
+
+	appendMember(student) {
+		student.klass = this;
+	}
+}
+module.exports = Class;
--- main/practice_10/student.js
+++ main/practice_10/student.js
@@ -1,1 +1,17 @@
+import Person from "./person.js";
 
+class Student extends Person {
+	constructor(id, name, age, klass) {
+		super(id, name, age);
+		this.klass = klass;
+	}
+
+	introduce() {
+		return (
+			`${super.introduce()} I am a Student. I am ` +
+			(this.klass.leader === this ? "Leader of" : "at") +
+			` Class ${this.klass.number}.`
+		);
+	}
+}
+module.exports = Student;
