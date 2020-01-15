9cf2aad08542f9bb34b2ca11da483107a1783e59
Asuu <suqinzheng_0624@163.com>
2020-01-15 23:13:22 +0800

    [zhengsuqin]feat-11: creat a class named Person and a class named Student and a class named teacher and a class named class

--- main/practice_11/class.js
+++ main/practice_11/class.js
@@ -1,1 +1,41 @@
+class Class {
+	constructor(klass) {
+		this.number = klass;
+	}
 
+	getDisplayName() {
+		return `Class ${this.number}`;
+	}
+
+	isIn(student) {
+		return student.klass === this;
+	}
+
+	assignLeader(student) {
+		if (this.isIn(student)) {
+			this.leader = student;
+			if (this.leaderListener) {
+        console.log(`I am ${this.leaderListener.name}. I know ${student.name} become Leader of Class ${student.klass.number}.`);
+			}
+		} else {
+			console.log("It is not one of us.");
+		}
+	}
+
+	appendMember(student) {
+		student.klass = this;
+		if (this.joinListener) {
+			console.log(`I am ${this.joinListener.name}. I know ${student.name} has joined Class ${student.klass.number}.`);
+		}
+	}
+
+	registerAssignLeaderListener(teacher) {
+		this.leaderListener = teacher;
+	}
+
+	registerJoinListener(teacher) {
+		this.joinListener = teacher;
+	}
+}
+
+module.exports = Class;
--- main/practice_11/teacher.js
+++ main/practice_11/teacher.js
@@ -1,1 +1,23 @@
+import Person from "./person.js";
 
+class Teacher extends Person {
+	constructor(id, name, age, klasses) {
+		super(id, name, age);
+		this.klasses = klasses;
+	}
+
+	introduce() {
+		let klassNum;
+		if (this.klasses) {
+			klassNum = this.klasses.map(klass => klass.number).join(", ");
+		}
+		return `${super.introduce()} I am a Teacher. I teach ` + (this.klasses ? "Class " + klassNum : "No Class") + ".";
+	}
+
+	isTeaching(student) {
+		for (let i = 0; i < this.klassNum.length; i++) {
+      return this.klassNum[i].isIn(student);
+    }
+	}
+}
+module.exports = Teacher;
