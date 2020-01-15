b6f421448876e23bfd747f9bb96e1fbc73ce1c82
Asuu <suqinzheng_0624@163.com>
2020-01-15 22:16:29 +0800

    [zhengsuqin]feat-8: creat a class named Person and a class named Student and a class named teacher and a class named class

--- main/practice_8/class.js
+++ main/practice_8/class.js
@@ -1,1 +1,20 @@
+class Class {
+	constructor(klass) {
+		this.number = klass;
+	}
 
+	getDisplayName() {
+		return `Class ${this.number}`;
+	}
+
+	isIn(student) {
+		return student.klass.number === this.number;
+	}
+
+	assignLeader(student) {
+		if (this.isIn(student)) {
+			this.leader = student;
+		}
+	}
+}
+module.exports = Class;
