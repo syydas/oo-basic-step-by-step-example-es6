db356174d6ca79d976b72918b3c3eda221364ea0
Asuu <suqinzheng_0624@163.com>
2020-01-15 21:54:48 +0800

    [zhengsuqin]feat-5,6: creat a class named Person and a class named Student and a class named Worker and a class named teacher

--- main/practice_6/teacher.js
+++ main/practice_6/teacher.js
@@ -1,1 +1,13 @@
+import Person from "./person.js";
 
+class Teacher extends Person {
+	constructor(name, age, klass) {
+		super(name, age);
+		this.klass = klass;
+	}
+
+	introduce() {
+		return `${super.introduce()} I am a Teacher. I teach ${this.klass ? "Class " + this.klass : "No Class"}.`;
+	}
+}
+module.exports = Teacher;
